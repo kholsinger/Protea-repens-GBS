c6be91594fea341b792942c6484a930035ae98ed
kholsinger <keholsinger@gmail.com>
2014-07-09 07:38:19 -0400

    Population locations, revisions to ibd
    IBD calculations revised to use (1-f)/f as distance
    
    Revised plotting scheme

--- ibd.R
+++ ibd.R
@@ -1,3 +1,8 @@
+require(ggplot2)
+require(maps)
+require(mapdata)
+require(plyr)
+
 rm(list=ls())
 
 strip <- function(x) {
@@ -30,7 +35,7 @@ get.freqs <- function(x) {
   p
 }
 
-get.ibd <- function(x, y, p) {
+get.ibd.dist <- function(x, y) {
   n.loci <- length(x)
   f <- 0.0
   k <- 0
@@ -40,14 +45,36 @@ get.ibd <- function(x, y, p) {
       q.1 <- 1.0 - p.1
       p.2 <- y[i]
       q.2 <- 1.0 - p.2
-      f <- f + (1.0 - (p.1*q.2 + q.1*p.2)/(2.0*p[i]*(1.0-p[i])))
+      f <- f + (p.1*p.2 + q.1*q.2)
       k <- k + 1
     }
   }
-  f/k
+  f <- f/k
+  f <- (1-f)/f
+  f
+}
+
+set.colors <- function(x, min.alpha=0.1, max.alpha=0.9) {
+  min.x <- min(x)
+  max.x <- max(x)
+  alpha <- (max.alpha-min.alpha)*(x - min.x)/(max.x - min.x) + min.alpha
+  color <- rgb(1.0, 0.0, 0.0, alpha)
+  color
+}
+
+set.colors.rb <- function(x, y) {
+  min.x <- min(x)
+  max.x <- max(x)
+  min.y <- min(y)
+  max.y <- max(y)
+  red <- 1.0 - (x - min.x)/(max.x - min.x)
+  blue <- 1.0 - (y - min.y)/(max.y - min.y)
+  color <- rgb(red, 0.0, blue)
+  color
 }
 
 dat <- read.marker.data("loci_20.csv")
+locs <- read.csv("pop_locations.csv", header=TRUE)
 
 p <- get.freqs(dat$markers[,1:dat$n.loci])
 p.indiv <- matrix(nrow=dat$n.indivs, ncol=dat$n.loci)
@@ -60,20 +87,105 @@ for (i in 1:dat$n.indivs) {
 }
 f.dist <- matrix(nrow=dat$n.indivs, ncol=dat$n.indivs)
 cat("Calculating pairwise ibd distances...")
-for (i in 1:dat$n.indivs) {
+for (i in 1:(dat$n.indivs-1)) {
   f.dist[i,i] <- 0.0
   cat("\n from individual ", i, sep="")
   for (j in (i+1):dat$n.indivs) {
     if ((j %% 50) == 0) {
       cat(".", sep="")
     }
-    f.dist[i,j] <- 1.0 - get.ibd(p.indiv[i,],
-                                 p.indiv[j,],
-                                 p)
+    f.dist[i,j] <- get.ibd.dist(p.indiv[i,],
+                                p.indiv[j,])
     f.dist[j,i] <- f.dist[i,j]
   }
 }
+f.dist[dat$n.indivs,dat$n.indivs] <- 0.0
 cat("\n")
-print(min(f.dist[,]))
-print(max(f.dist[,]))
+
+f.mds <- cmdscale(f.dist)
+
+for.plot <- data.frame(pop=dat$markers$pop,
+                       x=f.mds[,1],
+                       y=f.mds[,2],
+                       data="Individual trait")
+for.plot$pop <- factor(for.plot$pop,
+                       levels=c("ALC",
+                                "LOE",
+                                "KAR",
+                                "BAV",
+                                "UNI",
+                                "SWA",
+                                "GAR",
+                                "KSW",
+                                "ANY",
+                                "POT",
+                                "BRD",
+                                "RND",
+                                "MGU",
+                                "KLM",
+                                "CER",
+                                "RIV",
+                                "BAN",
+                                "CDB",
+                                "VAN"))
+means <- ddply(for.plot, c("pop"), summarize, X=mean(x), Y=mean(y))
+means$pop<- factor(for.plot$pop,
+                   levels=c("ALC",
+                            "LOE",
+                            "KAR",
+                            "BAV",
+                            "UNI",
+                            "SWA",
+                            "GAR",
+                            "KSW",
+                            "ANY",
+                            "POT",
+                            "BRD",
+                            "RND",
+                            "MGU",
+                            "KLM",
+                            "CER",
+                            "RIV",
+                            "BAN",
+                            "CDB",
+                            "VAN"))
+means$colors.1 <- set.colors(means$X, min.alpha=0.2, max.alpha=1.0)
+means$colors.2 <- set.colors(means$Y, min.alpha=0.2, max.alpha=1.0)
+means$colors.3 <- set.colors.rb(means$X, means$Y)
+means$data <- "Population mean"
+
+colors <- numeric(length(means$pop))
+for (i in 1:length(means$pop)) {
+  colors[i] <- rgb(i-1, 0, 9, maxColorValue=length(means$pop)-1)
+}
+
+dev.new()
+p <- ggplot(for.plot, aes(x=x, y=y, color=pop, size=data)) +
+     geom_point(alpha=0.8) +
+     geom_point(data=means, aes(x=X, y=Y, color=pop, size=data)) +
+     xlab("MDS f-dist Axis 1") +
+     ylab("MDS f-dist Axis 2") +
+     scale_color_manual(values=colors) +
+     guides(fill=guide_legend(reverse=TRUE))
+print(p)
+ggsave("MDS-f-dist-repens.pdf")
+
+for.plot <- merge(means, locs, by.x="pop", by.y="NAME")
+
+dev.new()
+par(mfrow=c(2,1))
+map("worldHires", "South Africa", col="gray95", fill=TRUE,
+    xlim=c(17.5,26.5), ylim=c(-35,-31))
+with(for.plot, points(long, lat, col=colors.1, pch=16))
+title(main="MDS f-dist Axis 1")
+map("worldHires", "South Africa", col="gray95", fill=TRUE,
+    xlim=c(17.5,26.5), ylim=c(-35,-31))
+with(for.plot, points(long, lat, col=colors.2, pch=16))
+title(main="MDS f-dist Axis 2")
+
+dev.new()
+map("worldHires", "South Africa", col="gray95", fill=TRUE,
+    xlim=c(17.5,26.5), ylim=c(-35,-31))
+with(for.plot, points(long, lat, col=colors.3, pch=16))
+title(main="MDS f-dist Axis 1 (red) + Axis 2 (blue)")
 
